cc200b51a848187f3d86b284a0db7edfe43c39c4
jacobkim9881 <jacobkim9881@gmail.com>
2021-02-13 18:48:33 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+	 <form id='put-email'>
+		 <input type='text' />
+		 <input type='submit' />
+	 </form>
+
+    </div>
+  );
+}
+
+export default App;
